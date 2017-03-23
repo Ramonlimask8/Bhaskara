03e7f031b79e604435e79fd6e49fc57b5e7051c2
Ramon Ferreira <ramonlima0912@gmail.com>
2017-03-23 20:33:51 -0300

    Solução do Bhaskara às avessas usando o NexLine

--- src/bhaskara/Bhaskara.java
+++ src/bhaskara/Bhaskara.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Bhaskara {
 
-    //ENTRADA
+    //PROGRAMA DO Bhaskara às avessas
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
         System.out.print("x1: ");
-        double x1 = leitor.nextDouble();
+        double x1 = Double.parseDouble(leitor.nextLine());
         System.out.print("x2: ");
-        double x2 = leitor.nextDouble();
+        double x2 = Double.parseDouble(leitor.nextLine());
         System.out.print("c: ");
-        double c = leitor.nextDouble();
-
+        double c = Double.parseDouble(leitor.nextLine());
+       
         // PROCESSAMENTO
         double s = x1 + x2;
         double p = x1 * x2;
